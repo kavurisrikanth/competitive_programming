a43073a75393d809030599306fa449dfe185064d
Srikanth Kavuri <ksrs9312@gmail.com>
2018-06-26 09:17:39 +0530

    Apple and ProductExcept done.

---
+++ Week 1/Day 1/ProductExcept.java
@@ -0,0 +1,99 @@
+public class ProductExcept {
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+
+        if (intArray.length < 2) {
+            throw new IllegalArgumentException("Need at least 2 numbers.");
+        }
+
+        int[] ans = new int[intArray.length];
+
+        // for each integer, we find the product of all the integers
+        // before it, storing the total product so far each time
+        int product = 1;
+        for (int i = 0; i < intArray.length; i++) {
+            ans[i] = product;
+            product *= intArray[i];
+        }
+
+        // for each integer, we find the product of all the integers
+        // after it. since each index in products already has the
+        // product of all the integers before it, now we're storing
+        // the total product of all other integers
+        product = 1;
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            ans[i] *= product;
+            product *= intArray[i];
+        }
+
+        return ans;
+    }
+
+    private static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    @Test
+    public void smallArrayTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {1, 2, 3});
+        final int[] expected = new int[] {6, 3, 2};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void longArrayTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {8, 2, 4, 3, 1, 5});
+        final int[] expected = new int[] {120, 480, 240, 320, 960, 192};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasOneZeroTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {6, 2, 0, 3});
+        final int[] expected = new int[] {0, 0, 36, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasTwoZerosTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {4, 0, 9, 1, 0});
+        final int[] expected = new int[] {0, 0, 0, 0, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void oneNegativeNumberTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {-3, 8, 4});
+        final int[] expected = new int[] {32, -12, -24};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void allNegativeNumbersTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {-7, -1, -4, -2});
+        final int[] expected = new int[] {-8, -56, -14, -28};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyArrayTest() {
+        getProductsOfAllIntsExceptAtIndex(new int[] {});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithOneNumberTest() {
+        getProductsOfAllIntsExceptAtIndex(new int[] {1});
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
+
